6ebf04b95906a5df7d9e722cd670eae7af1d469e
Yuda <mahendrayudapradana@mail.ugm.ac.id>
2023-08-26 03:01:23 +0700

    update bmi

--- app/src/main/java/com/example/pertemuan2/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan2/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.pertemuan2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
+import android.widget.EditText
 import android.widget.Toast
 import com.example.pertemuan2.databinding.ActivityMainBinding
+import java.math.BigDecimal
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +19,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
-            textNumber.text = number.toString()
-            btnCount.setOnClickListener {
-                number ++
-                textNumber.text = number.toString()
+            binding.btnCount.setOnClickListener {
+                val weightString = binding.weight.text.toString()
+                val heightString = binding.height.text.toString()
+
+                val weightcal = BigDecimal(weightString)
+                val heightcal = BigDecimal(heightString)
+
+                val bmi = weightcal/(heightcal*heightcal)
+                textNumber.text = bmi.toString()
             }
+
             btnToast.setOnClickListener {
-                Toast.makeText(this@MainActivity, "Count $number",
-                Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Count ${textNumber.text}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
     }
+
 }
